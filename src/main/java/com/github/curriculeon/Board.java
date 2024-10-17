54540f1b1834e1c0c5eda48bdd275e33a16b8a0e
Mohammed N <mnaser11218@gmail.com>
2024-10-17 12:18:26 -0400

    completed all test except diagnal

--- src/main/java/com/github/curriculeon/Board.java
+++ src/main/java/com/github/curriculeon/Board.java
@@ -4,23 +4,88 @@ package com.github.curriculeon;
  * @author leon on 6/22/18.
  */
 public class Board {
+    public Character[][] matrix = new Character[][]{};
     public Board(Character[][] matrix) {
+        this.matrix = matrix;
     }
 
+//       this.board = new Board(new Character[][]{
+//        {'X', 'X', 'O'},
+//        {'O', 'X', 'X'},
+//        {'X', 'O', 'O'}
+//    })
+
     public Boolean isInFavorOfX() {
-        return null;
+        boolean results  =false;
+        int countDia = 0;
+        int countHor = 0;
+
+        for(int i=0; i<matrix.length; i++){
+
+            for(int j =0; j< matrix.length; j++){
+                if(matrix[i][j].equals('X')){
+                    countDia++;
+                    if(countDia == 3){
+                        return true;
+                    }
+                } if(matrix[j][i].equals('X')){
+                    countHor++;
+                    if(countHor == 3){
+                        return true;
+                    }
+                }
+
+            }
+            countDia = 0;
+            countHor=0;
+
+
+        }
+
+
+        return results;
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        boolean results  =false;
+        int countDia = 0;
+        int countHor = 0;
+
+        for(int i=0; i<matrix.length; i++) {
+
+            for (int j = 0; j < matrix.length; j++) {
+                if (matrix[i][j].equals('O')) {
+                    countDia++;
+                    if (countDia == 3) {
+                        return true;
+                    }
+                }
+                if (matrix[j][i].equals('O')) {
+                    countHor++;
+                    if (countHor == 3) {
+                        return true;
+                    }
+                }
+
+            }
+            countDia = 0;
+            countHor = 0;
+        }
+        return results;
     }
 
     public Boolean isTie() {
-        return null;
+        return !isInFavorOfO() && !isInFavorOfX();
     }
 
     public String getWinner() {
-        return null;
+        if(isInFavorOfX()){
+            return "X";
+        }else if(isInFavorOfO()){
+            return "O";
+        }else{
+            return "";
+        }
     }
 
 }
